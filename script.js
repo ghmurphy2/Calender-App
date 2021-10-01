a0b8292698f24ade91bf6530c9c3d0b56f4525b0
Gavin Murphy <gavinhmurphy@gmail.com>
2021-09-30 17:43:22 -0700

    timer functiogit add . ls

--- script.js
+++ script.js
@@ -4,8 +4,17 @@
 // red out past blocks
 // allow block input text
 // run timer on blocks? alert popups at intervels?
-var hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+var hours = ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
 var hourEls = [];
 var container = $(".container");
-var now = moment();
+var now = momentNow();
 var timeEl = $("#currentDay");
+
+// establish timer on load
+setInterval(displayTime, 1000);
+//establsih timer for display
+function displayTime() {
+    let todayOnInterval = moment();
+    let timeNow = todayOnInterval.format('HH[:]mm[:]ss');
+    $('#currentTime').text(timeEl);
+}
